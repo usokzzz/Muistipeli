790343bd4c3f30880848a3993246622dd5752b46
Eeelii <kissiebb@gmail.com>
2026-05-27 23:41:47 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,10 +1,20 @@
-import { createBoard } from './board.js';
+import { createBoard, restartGame } from './board.js';
+let currentCardCount;
 
 document.addEventListener('DOMContentLoaded', () => {
     const cardCount = parseInt(prompt("Syötä korttien määrä (parillinen luku):"), 10);
+
     if (cardCount % 2 !== 0) {
         alert("Korttien määrän täytyy olla parillinen luku.");
         return;
     }
+
+    currentCardCount = cardCount;
+
     createBoard(cardCount);
+
+    document.getElementById('restart-button')
+        .addEventListener('click', () => {
+            restartGame(currentCardCount);
+        });
 });
